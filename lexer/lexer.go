296a89a46062258aedba048c4c2bad6d5ec019dc
Maximos Nikiforakis <nikiforos@live.co.uk>
2025-01-23 10:23:10 +0200

    Extends lexer's recognized tokens

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -31,6 +31,8 @@ func (l *Lexer) readChar() {
 func (l *Lexer) NextToken() token.Token {
   var tok token.Token
 
+  l.skipWhitespace()
+
   switch l.currentChar {
   case '=':
     tok = newToken(token.ASSIGN, '=')
@@ -51,12 +53,54 @@ func (l *Lexer) NextToken() token.Token {
   case 0:
     tok.Literal = ""
     tok.Type = token.EOF
+  default:
+    if isLetter(l.currentChar) {
+      tok.Literal = l.readIdentifier()
+      tok.Type = token.LookupIdent(tok.Literal)
+      return tok
+    } else if isDigit(l.currentChar) {
+      tok.Literal = l.readNumber()
+      tok.Type = token.INT
+      return tok
+    } else {
+      tok = newToken(token.ILLEGAL, l.currentChar)
+    }
   }
 
   l.readChar()
   return tok
 }
 
+func (l *Lexer) readIdentifier() string {
+  startPosition := l.currentPosition
+  for isLetter(l.currentChar) {
+    l.readChar()
+  }
+  return l.input[startPosition:l.currentPosition]
+}
+
+func (l *Lexer) readNumber() string {
+  startPosition := l.currentPosition
+  for isDigit(l.currentChar) {
+    l.readChar()
+  }
+  return l.input[startPosition:l.currentPosition]
+}
+
+func (l *Lexer) skipWhitespace() {
+  for l.currentChar == ' ' || l.currentChar == '\t' || l.currentChar == '\n' || l.currentChar == '\r' {
+    l.readChar()
+  }
+}
+
 func newToken(tokenType token.TokenType, char byte) token.Token {
   return token.Token{Type: tokenType, Literal: string(char)}
+}
+
+func isLetter(char byte) bool {
+  return 'a' <= char && char <= 'z' || 'A' <= char && char <= 'Z' || char == '_'
+}
+
+func isDigit(char byte) bool {
+  return '0' <= char && char <= '9'
 }
