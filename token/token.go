54bf67578947915ff5086625c8ea62327a3ed529
Maximos Nikiforakis <nikiforos@live.co.uk>
2025-01-23 10:22:33 +0200

    Adds keywords map and LookupIdent func to token package

--- token/token.go
+++ token/token.go
@@ -10,7 +10,7 @@ type Token struct {
 const (
 	ILLEGAL = "ILLEGAL"
 	EOF     = "EOF"
-  
+ 
   IDENT = "IDENT"
   INT = "INT"
 
@@ -28,3 +28,15 @@ const (
   FUNCTION = "FUNCTION"
   LET = "LET"
 )
+
+var keywords = map[string]TokenType{
+  "fn": FUNCTION,
+  "let": LET,
+}
+
+func LookupIdent(ident string) TokenType {
+  if tok, ok := keywords[ident]; ok {
+    return tok
+  }
+  return IDENT
+}
