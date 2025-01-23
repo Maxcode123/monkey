3d054850f8d5da8d087582cfec0ea70165e8d37f
Maximos Nikiforakis <nikiforos@live.co.uk>
2025-01-23 20:36:20 +0200

    Adds tests for eq and not eq tokens

--- lexer/lexer_test.go
+++ lexer/lexer_test.go
@@ -24,6 +24,9 @@ func TestNextToken(t *testing.T) {
   } else {
     return false;
   }
+
+  10 == 10;
+  10 != 9;
   `
 
 	tests := []struct {
@@ -56,14 +59,16 @@ func TestNextToken(t *testing.T) {
 		{token.LET, "let"}, {token.IDENT, "result"}, {token.ASSIGN, "="}, {token.IDENT, "add"},
 		{token.LPAREN, "("}, {token.IDENT, "five"}, {token.COMMA, ","}, {token.IDENT, "ten"},
 		{token.RPAREN, ")"}, {token.SEMICOLON, ";"},
-    {token.BANG, "!"}, {token.MINUS, "-"}, {token.SLASH, "/"}, {token.ASTERISK, "*"},
-    {token.INT, "5"}, {token.SEMICOLON, ";"}, {token.INT, "5"}, {token.LT, "<"},
-    {token.INT, "10"}, {token.GT, ">"}, {token.INT, "5"}, {token.SEMICOLON, ";"},
-    {token.IF, "if"}, {token.LPAREN, "("}, {token.INT, "5"}, {token.LT, "<"},
-    {token.INT, "10"}, {token.RPAREN, ")"}, {token.LBRACE, "{"}, {token.RETURN, "return"},
-    {token.TRUE, "true"}, {token.SEMICOLON, ";"}, {token.RBRACE, "}"},
-    {token.ELSE, "else"}, {token.LBRACE, "{"}, {token.RETURN, "return"},
-    {token.FALSE, "false"}, {token.SEMICOLON, ";"}, {token.RBRACE, "}"},
+		{token.BANG, "!"}, {token.MINUS, "-"}, {token.SLASH, "/"}, {token.ASTERISK, "*"},
+		{token.INT, "5"}, {token.SEMICOLON, ";"}, {token.INT, "5"}, {token.LT, "<"},
+		{token.INT, "10"}, {token.GT, ">"}, {token.INT, "5"}, {token.SEMICOLON, ";"},
+		{token.IF, "if"}, {token.LPAREN, "("}, {token.INT, "5"}, {token.LT, "<"},
+		{token.INT, "10"}, {token.RPAREN, ")"}, {token.LBRACE, "{"}, {token.RETURN, "return"},
+		{token.TRUE, "true"}, {token.SEMICOLON, ";"}, {token.RBRACE, "}"},
+		{token.ELSE, "else"}, {token.LBRACE, "{"}, {token.RETURN, "return"},
+		{token.FALSE, "false"}, {token.SEMICOLON, ";"}, {token.RBRACE, "}"},
+		{token.INT, "10"}, {token.EQ, "=="}, {token.INT, "10"}, {token.SEMICOLON, ";"},
+		{token.INT, "10"}, {token.NOT_EQ, "!="}, {token.INT, "9"}, {token.SEMICOLON, ";"},
 	}
 
 	l := New(input)
