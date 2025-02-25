93dc285aa7127f2a647e010c8fc6a9137c243558
Maximos Nikiforakis <nikiforos@live.co.uk>
2025-02-25 10:19:27 +0200

    Evaluates booleans

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -1,30 +1,45 @@
 package evaluator
 
 import (
-  "monkey/ast"
-  "monkey/object"
+	"monkey/ast"
+	"monkey/object"
+)
+
+var (
+	TRUE  = &object.Boolean{Value: true}
+	FALSE = &object.Boolean{Value: false}
 )
 
 func Eval(node ast.Node) object.Object {
-  switch node := node.(type) {
- 
-  case *ast.Program:
-    return evalStatements(node.Statements)
-  case *ast.ExpressionStatement:
-    return Eval(node.Expression)
-  case *ast.IntegerLiteral:
-    return &object.Integer{Value: node.Value}
-  }
+	switch node := node.(type) {
 
-  return nil
+	case *ast.Program:
+		return evalStatements(node.Statements)
+	case *ast.ExpressionStatement:
+		return Eval(node.Expression)
+	case *ast.IntegerLiteral:
+		return &object.Integer{Value: node.Value}
+	case *ast.Boolean:
+		return nativeToBooleanObject(node.Value)
+	}
+
+	return nil
 }
 
 func evalStatements(stmts []ast.Statement) object.Object {
-  var result object.Object
+	var result object.Object
 
-  for _, stmt := range stmts {
-    result = Eval(stmt)
-  }
+	for _, stmt := range stmts {
+		result = Eval(stmt)
+	}
 
-  return result
+	return result
+}
+
+func nativeToBooleanObject(input bool) *object.Boolean {
+	if input {
+		return TRUE
+	}
+
+	return FALSE
 }
